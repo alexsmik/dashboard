0e131fcadfb6131d3a14d2e9b833dee999a543bf
alexsmik <alexsmik@yandex.ru>
2022-06-22 14:07:33 +0300

    added data

---
+++ data/dummy.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import avatar from './avatar.jpg';
+import avatar2 from './avatar2.jpg';
+import avatar3 from './avatar3.png';
+import avatar4 from './avatar4.jpg';
+import product1 from './product1.jpg';
+import product2 from './product2.jpg';
+import product3 from './product3.jpg';
+import product4 from './product4.jpg';
+import product5 from './product5.jpg';
+import product6 from './product6.jpg';
+import product7 from './product7.jpg';
+import product8 from './product8.jpg';
+
+export const gridOrderImage = (props) => (
+  <div>
+    <img
+      className="rounded-xl h-20 md:ml-3"
+      src={props.ProductImage}
+      alt="order-item"
+    />
+  </div>
+);
+
+export const gridOrderStatus = (props) => (
+  <button
+    type="button"
+    style={{ background: props.StatusBg }}
+    className="text-white py-1 px-2 capitalize rounded-2xl text-md"
+  >
+    {props.Status}
+  </button>
+);
