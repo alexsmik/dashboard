9e9fa25472ec011a791c7eb06345e50e2b6514bd
alexsmik <alexsmik@yandex.ru>
2023-03-10 13:31:03 +0300

    upd

---
+++ client/src/state/api.js
@@ -0,0 +1,33 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const api = createApi({
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+    reducerPath: "adminApi",
+    tagTypes: [
+        "User",
+        "Products",
+        "Customers",
+        "Transactions",
+        "Geography",
+        "Sales",
+        "Admins",
+        "Performance",
+        "Dashboard",
+    ],
+    endpoints: (build) => ({
+        getUser: build.query({
+            query: (id) => `general/user/${id}`,
+            providesTags: ["User"],
+        }),
+        getDashboard: build.query({
+            query: () => "general/dashboard",
+            providesTags: ["Dashboard"],
+        }),
+    }),
+});
+
+export const {
+    useGetUserQuery,
+    useGetDashboardQuery,
+    // useGetSalesQuery
+} = api;
