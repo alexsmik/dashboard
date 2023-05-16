bc6a94e38c38734f4ab9515ad7d1d5d2d5cca153
alexsmik <alexsmik@yandex.ru>
2023-05-16 15:14:19 +0300

    upd

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,59 +1,62 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
-  reducerPath: "adminApi",
+  // baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://admin-panel-totun.onrender.com/',
+  }),
+  reducerPath: 'adminApi',
   tagTypes: [
-    "User",
-    "Products",
-    "Customers",
-    "Transactions",
-    "Geography",
-    "Sales",
-    "Admins",
-    "Performance",
-    "Dashboard",
+    'User',
+    'Products',
+    'Customers',
+    'Transactions',
+    'Geography',
+    'Sales',
+    'Admins',
+    'Performance',
+    'Dashboard',
   ],
   endpoints: (build) => ({
     getUser: build.query({
       query: (id) => `general/user/${id}`,
-      providesTags: ["User"],
+      providesTags: ['User'],
     }),
     getProducts: build.query({
-      query: () => "client/products",
-      providesTags: ["Products"],
+      query: () => 'client/products',
+      providesTags: ['Products'],
     }),
     getCustomers: build.query({
-      query: () => "client/customers",
-      providesTags: ["Customers"],
+      query: () => 'client/customers',
+      providesTags: ['Customers'],
     }),
     getTransactions: build.query({
       query: ({ page, pageSize, sort, search }) => ({
-        url: "client/transactions",
-        method: "GET",
+        url: 'client/transactions',
+        method: 'GET',
         params: { page, pageSize, sort, search },
       }),
-      providesTags: ["Transactions"],
+      providesTags: ['Transactions'],
     }),
     getGeography: build.query({
-      query: () => "client/geography",
-      providesTags: ["Geography"],
+      query: () => 'client/geography',
+      providesTags: ['Geography'],
     }),
     getSales: build.query({
-      query: () => "sales/sales",
-      providesTags: ["Sales"],
+      query: () => 'sales/sales',
+      providesTags: ['Sales'],
     }),
     getAdmins: build.query({
-      query: () => "management/admins",
-      providesTags: ["Admins"],
+      query: () => 'management/admins',
+      providesTags: ['Admins'],
     }),
     getUserPerformance: build.query({
       query: (id) => `management/performance/${id}`,
-      providesTags: ["Performance"],
+      providesTags: ['Performance'],
     }),
     getDashboard: build.query({
-      query: () => "general/dashboard",
-      providesTags: ["Dashboard"],
+      query: () => 'general/dashboard',
+      providesTags: ['Dashboard'],
     }),
   }),
 });
