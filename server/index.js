bdabbc1320f27fab6237a70b3e3559bfa6689557
alexsmik <alexsmik@yandex.ru>
2023-05-24 12:16:38 +0300

    upd

--- server/index.js
+++ server/index.js
@@ -35,6 +35,7 @@ app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.set('trust proxy', 1);
 app.use(cors());
 
 // production mode build(dist) frontend in backend
